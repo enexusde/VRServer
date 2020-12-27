0e76c8ef19fe64975b6695f389637da54f38ad36
enexusde <github@e-nexus.de>
2020-12-27 20:05:22 +0100

    Enable OSGi implementation.

---
+++ src/main/java/de/e_nexus/vr/server/osgi/VRServerServiceDelegate.java
@@ -0,0 +1,24 @@
+package de.e_nexus.vr.server.osgi;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.cdi.annotations.Service;
+
+import de.e_nexus.vr.server.VRServer;
+import de.e_nexus.vr.server.osgi.inter.VRServerService;
+
+@Service
+@ApplicationScoped
+public class VRServerServiceDelegate {
+
+	@Inject
+	BundleContext bc = null;
+
+	public VRServer getVRServer() {
+		ServiceReference<VRServerService> ref = bc.getServiceReference(VRServerService.class);
+		return (VRServer) bc.getService(ref);
+	}
+}
